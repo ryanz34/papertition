116c65b7d2a98f81471d890d6bf18d7fab018481
Ryan <ryanzhang0112@gmail.com>
2021-01-16 16:35:45 -0500

    New test case + pageid

--- backend/pdf_parser.py
+++ backend/pdf_parser.py
@@ -3,6 +3,7 @@ import paper_cv
 import pdf2image
 from google.cloud import vision
 import os
+import math
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.abspath("papertition-c318e0fafafa.json")
 
@@ -18,6 +19,28 @@ class Page:
     def __str__(self):
         return self.__repr__
 
+class Text:
+    def __init__(self, bounds, num):
+        self.bounds = [(v.x, v.y) for v in bounds]
+        self.num = num
+        self.x, self.y = self.compute_avg()
+        self.dist = self.compute_dist()
+
+    def compute_avg(self):
+        min_x = self.bounds[0][0]
+        min_y = self.bounds[0][1]
+
+        for v in range(1, len(self.bounds)):
+            min_x = min(min_x, self.bounds[v][0])
+            min_y = min(min_y, self.bounds[v][1])
+
+        return min_x, min_y
+
+    def compute_dist(self):
+        return math.hypot(self.x, self.y)
+
+def dist(num : Text):
+    return num.dist
 
 def import_pages(path):
     """
@@ -40,11 +63,23 @@ def parse(path):
 
         response = client.document_text_detection(image=image)
         labels = response.text_annotations
+        numbers = []
+
         for l in labels:
-            if l.description.strip().strip('\n') in {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}:
-                pages[p].pdf_id = int(l.description.strip().strip('\n'))
-                pages[p].page = int(l.description.strip().strip('\n'))
-                break
+            if l.description.strip() in {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}:
+                numbers.append(Text(l.bounding_poly.vertices, int(l.description.strip())))
+
+        if len(numbers) > 0:
+            pid = numbers[0]
+            page = numbers[0]
+
+            for n in numbers:
+                pid = min(pid, n, key=dist)
+                page = max(page, n, key=dist)
+
+            pages[p].pdf_id = pid.num
+            pages[p].page = page.num
+
     print(pages)
 
     # Next let the user reorder it if they want to
