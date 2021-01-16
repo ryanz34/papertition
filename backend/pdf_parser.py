b66e47f13f840ec6d12384342c0cd9ad7962d69d
Henry T <henry@henrytu.me>
2021-01-16 02:33:44 -0500

    Updated dependencies

--- backend/pdf_parser.py
+++ backend/pdf_parser.py
@@ -1,9 +1,20 @@
 import pdf2image
+import numpy
+
+def import_images(path):
+    """
+    Imports the images at a path as a list of numpy matracies
+    :param path:
+    :return:
+    """
+
+    return [numpy.asarray(image) for image in pdf2image.convert_from_path(path)]
 
 def parse(path):
-    images = pdf2image.convert_from_path(path)
+    images = import_images(path)
 
     print(images)
+
 
 if __name__ == '__main__':
     parse("../test_data/test.pdf")
