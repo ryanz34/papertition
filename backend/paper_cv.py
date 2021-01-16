5737e521c4d4fd4978195096068d78a3e7a45724
Ryan <ryanzhang0112@gmail.com>
2021-01-16 10:58:40 -0500

    Change vertical/horizontal threshold and add auto contrast

--- backend/paper_cv.py
+++ backend/paper_cv.py
@@ -25,7 +25,19 @@ def length(x1, y1, x2, y2):
     return math.hypot(dx, dy)
 
 
-def detect_lines(gray, outname, debug=False):
+def detect_lines(img, outname, debug=False, vhthreshold=0.05):
+
+    lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+
+    l, a, b = cv2.split(lab)
+
+    clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
+    cl = clahe.apply(l)
+
+    limg = cv2.merge((cl, a, b))
+
+    gray = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
+
     edges = cv2.Canny(gray, 50, 150, apertureSize=3)
 
     if debug:
@@ -45,14 +57,14 @@ def detect_lines(gray, outname, debug=False):
 
     for i in range(a):
 
-        if vertical(lines[i][0][0], lines[i][0][1], lines[i][0][2], lines[i][0][3], 0.005):
+        if vertical(lines[i][0][0], lines[i][0][1], lines[i][0][2], lines[i][0][3], vhthreshold):
 
             if length(lines[i][0][0], lines[i][0][1], lines[i][0][2], lines[i][0][3]) > length(v[0][0], v[0][1], v[1][0], v[1][1]):
 
                 vdetect = True
                 v = (lines[i][0][0], lines[i][0][1]), (lines[i][0][2], lines[i][0][3])
 
-        elif horizontal(lines[i][0][0], lines[i][0][1], lines[i][0][2], lines[i][0][3], 0.005):
+        elif horizontal(lines[i][0][0], lines[i][0][1], lines[i][0][2], lines[i][0][3], vhthreshold):
 
             if max(lines[i][0][1], lines[i][0][1]) < max(h[0][1], h[1][1]):
                 hdetect = True
@@ -60,6 +72,7 @@ def detect_lines(gray, outname, debug=False):
 
     if not vdetect or not hdetect:
         raise Exception("Cannot detect valid vertical or horizontal line")
+
 
     x = (v[0][0] + v[1][0]) // 2
     y = (h[0][1] + h[1][1]) // 2
